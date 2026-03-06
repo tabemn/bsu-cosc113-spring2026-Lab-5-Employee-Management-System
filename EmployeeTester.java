216f298cfde93ed7cbf0b4e1437c348ae8df4c9b
tabemn <tabemn0720@students.bowiestate.edu>
2026-03-05 21:45:46 -0500

    "LAB 5 COMPLETION"

---
+++ EmployeeTester.java
@@ -0,0 +1,55 @@
+class Employee {
+    protected String name;
+
+    protected Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double calculateSalary() {
+        return 0.0;
+    }
+}
+
+class FullTimeEmployee extends Employee {
+    private double monthlySalary;
+
+    public FullTimeEmployee(String name, double monthlySalary) {
+        super(name);
+        this.monthlySalary = monthlySalary;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return monthlySalary;
+    }
+}
+
+class PartTimeEmployee extends Employee {
+    private double hourlyRate;
+    private int hoursWorked;
+
+    public PartTimeEmployee(String name, double hourlyRate, int hoursWorked) {
+        super(name);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return hourlyRate * hoursWorked;
+    }
+}
+
+public class EmployeeTester {
+    public static void main(String[] args) {
+        Employee fullTime = new FullTimeEmployee("Alice", 5000.0);
+        Employee partTime = new PartTimeEmployee("Bob", 20.0, 160);
+
+        System.out.println(fullTime.getName() + "'s salary: " + fullTime.calculateSalary());
+        System.out.println(partTime.getName() + "'s salary: " + partTime.calculateSalary());
+    }
+}
